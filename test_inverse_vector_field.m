33c4f179001a77109f7b423b25e7396ff5bcbe7b
Tom <tjwdraper@gmail.com>
2024-03-18 17:35:39 +0100

    Added image for github repo

--- test_inverse_vector_field.m
+++ test_inverse_vector_field.m
@@ -42,27 +42,19 @@ endfor
 [dudy, dudx] = gradient(squeeze(motion(:,:,1)));
 [dvdy, dvdx] = gradient(squeeze(motion(:,:,2)));
 
-divu = dudx + dvdy;
-
 jacu = (1 + dudx) .* (1 + dvdy) - dudy .* dvdx;
 
 [dudyinv, dudxinv] = gradient(squeeze(invmotion(:,:,1)));
 [dvdyinv, dvdxinv] = gradient(squeeze(invmotion(:,:,2)));
 
-divuinv = dudxinv + dvdyinv;
-
 jacuinv = (1 + dudxinv) .* (1 + dvdyinv) - dudyinv .* dvdxinv;
 
 %%
 figure(); 
-subplot(2,4,1); quiver(motion(end:-1:1,:,1), motion(end:-1:1,:,2), 0); axis off; title('input motion');
-subplot(2,4,2); imagesc(divu); colormap jet; axis off; colorbar; title('divergence');
-subplot(2,4,3); imagesc(jacu); colormap jet; axis off; colorbar; title('jacobian');
-subplot(2,4,4); imagesc(sqrt(dudx.^2 + dudy.^2 + dvdx.^2 + dvdy.^2)); colormap jet; axis off; colorbar; title('harmonic energy');
-subplot(2,4,5); quiver(invmotion(end:-1:1, :, 1), invmotion(end:-1:1, :, 2), 0); axis off; title('inverted motion');
-subplot(2,4,6); imagesc(divuinv); colormap jet; axis off; colorbar; title('divergence');
-subplot(2,4,7); imagesc(jacuinv); colormap jet; axis off; colorbar; title('jacobian');
-subplot(2,4,8); imagesc(sqrt(dudxinv.^2 + dudyinv.^2 + dvdxinv.^2 + dvdyinv.^2)); colormap jet; axis off; colorbar; title('harmonic energy');
+subplot(2,2,1); quiver(motion(end:-1:1,:,1), motion(end:-1:1,:,2), 0); axis off; title('input motion');
+subplot(2,2,2); imagesc(jacu); colormap jet; axis off; colorbar; title('jacobian');
+subplot(2,2,3); quiver(invmotion(end:-1:1, :, 1), invmotion(end:-1:1, :, 2), 0); axis off; title('inverted motion');
+subplot(2,2,4); imagesc(jacuinv); colormap jet; axis off; colorbar; title('jacobian');
 
 
 %% Warp a grid forward and then invert
